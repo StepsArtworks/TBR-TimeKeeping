06142d2ce93e36f48e7c527cf81da5965736fd52
Andre Hamman <hammanandre@gmail.com>
2025-02-21 12:54:40 +0200

    Update to latest

--- Backend-Server/routes/users.js
+++ Backend-Server/routes/users.js
@@ -3,12 +3,10 @@ const requiredparms = require("../middleware/requiredparms.js");
 
 
 // JWT secret key - in production this should be in environment variables
-const JWT_SECRET = 'your-secret-key';
 
 module.exports = function (app) {
 
 
-    // Middleware to verify JWT token
 
 
 
