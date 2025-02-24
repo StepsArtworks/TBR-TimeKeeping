1e1090c23473957285da2bad1a0ee475cdc9be76
Andre Hamman <hammanandre@gmail.com>
2025-02-24 16:18:47 +0200

    Update auth.js

--- Backend-Server/routes/auth.js
+++ Backend-Server/routes/auth.js
@@ -28,6 +28,7 @@ module.exports = function (app) {
 
                 // Compare password
                 const isValidPassword = await bcrypt.compare(password, user.password);
+                console.log(isValidPassword);
                 if (!isValidPassword) {
                     return res.status(401).json({
                         status: 401,
