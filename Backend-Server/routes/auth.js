630f19e782cbf70e161fc4a3b64a62e034d49ec8
StepsArtworks <105391959+StepsArtworks@users.noreply.github.com>
2025-02-25 15:20:05 +0200

    Add cors back

--- Backend-Server/routes/auth.js
+++ Backend-Server/routes/auth.js
@@ -50,7 +50,13 @@ module.exports = function (app) {
                 // Remove password from user object
                 delete user.password;
 
-
+   res.set({
+                    'Access-Control-Allow-Origin': 'https://tbrhub.com',
+                    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Accept',
+                    'Access-Control-Allow-Credentials': 'true'
+                });
+                
                 return res.status(200).json({
                     token,
                     user
