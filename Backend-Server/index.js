91d4b409f885a1d81c237b5bcf85b76bc66a44c0
Andre Hamman <hammanandre@gmail.com>
2025-03-03 13:50:29 +0200

    Update index.js

--- Backend-Server/index.js
+++ Backend-Server/index.js
@@ -7,7 +7,9 @@ const bodyParser = require('body-parser');
 const app = express();
 var port = 7777;
 app.use(cors({
-  origin: 'https://tbrhub.com'
+  origin: 'https://tbrhub.com',
+  credentials: true
+
 }));
 
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
