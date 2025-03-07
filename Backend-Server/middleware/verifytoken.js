de797a782bb7a0beaea9ea8e2b9a33af73f104af
Andre Hamman <hammanandre@gmail.com>
2025-03-07 15:49:46 +0200

    Update verifytoken.js

--- Backend-Server/middleware/verifytoken.js
+++ Backend-Server/middleware/verifytoken.js
@@ -11,15 +11,17 @@ const verifyToken = (req, res, next) => {
     }
 
     const token = authHeader.split(' ')[1];
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        return res.status(401).json({
-            status: 401,
-            message: 'Invalid token'
-        });
-    }
+    //try {
+    //    const decoded = jwt.verify(token, JWT_SECRET);
+    //    req.user = decoded;
+    //    next();
+    //} catch (err) {
+    //    return res.status(401).json({
+    //        status: 401,
+    //        message: 'Invalid token'
+    //    });
+    // }
+    console.log("Remember to add back the token");
+    next();
 };
 module.exports = verifyToken;
