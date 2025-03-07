acc0ee461c3df6e9f6617d0bc44b33850a3ddbde
Andre Hamman <hammanandre@gmail.com>
2025-03-07 15:30:19 +0200

    Update verifytoken.js

--- Backend-Server/middleware/verifytoken.js
+++ Backend-Server/middleware/verifytoken.js
@@ -1,5 +1,6 @@
 // Middleware to verify JWT token
 const verifyToken = (req, res, next) => {
+    console.log(req.headers);
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({
