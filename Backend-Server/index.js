e98227148ec5e8ae0e461aea3780dd7bbe37b991
Andre Hamman <22626910+hammanandre@users.noreply.github.com>
2025-03-07 15:14:24 +0200

    Update index.js

--- Backend-Server/index.js
+++ Backend-Server/index.js
@@ -1,31 +1,36 @@
 var fs = require("fs");
-var path = require('path');
-var cors = require('cors')
-var morgan = require('morgan');
-const express = require('express')
-const bodyParser = require('body-parser');
+var path = require("path");
+var cors = require("cors");
+var morgan = require("morgan");
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 var port = 7777;
-app.use(cors({
-  origin: 'https://tbrhub.com',
-  credentials: true
+app.use(
+  cors({
+    origin: "https://tbrhub.com",
+    credentials: true,
+  })
+);
 
-}));
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
+app.use(
+  morgan("combined", {
+    stream: accessLogStream,
+  })
+);
 
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
-  flags: 'a'
-})
-app.use(morgan('combined', {
-  stream: accessLogStream
-}))
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-require('./routes/auth.js')(app);
-require('./routes/projects.js')(app);
+require("./routes/auth.js")(app);
+require("./routes/projects.js")(app);
 
 console.log(`TBR Timekeeping - Listening on Port ${port}`);
 app.listen(port);
